c7ab5bf0cd200fbd5de2188555df48e89e61ef2b
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2024-06-20 18:20:30 -0500

    ewqg

--- relay.py
+++ relay.py
@@ -1,24 +1,46 @@
 import serial
 import socket
 import sys
+import time
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Invalid syntax: \"python3 relay.py <target>\"")
+	if len(sys.argv) != 3:
+		print("Invalid syntax: \"python3 relay.py <target> <port>\"")
 		exit(-1)
-	print("Loading socket ...", end="")
+	print("Loading serial connection ...", end="")
+	sys.stdout.flush()
+	ser = serial.Serial(sys.argv[2], 9600)
+	print("done.\nLoading socket ...", end="")
 	sys.stdout.flush()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print("done.\nFinding hostname and IP ...", end="")
-	sys.stdout.flush()
-	hn = socket.gethostname()
-	ip = socket.gethostbyname(hn)
-	print("done, host", hn, "ip", ip, "\nConnecting to target ...", end="")
+	print("done.\nConnecting to target ...", end="")
 	sys.stdout.flush()
 	try:
 		sock.connect((sys.argv[1], 7777))
 	except Exception as e:
 		print("error connecting to server:", str(e))
+		if input("Raise error? (y/n) ") == "y":
+			raise e
 		exit(-2)
 	print("done, ready to transmit data.")
-	
+
+	try:
+		while True:
+			# Read data from Arduino
+			if ser.in_waiting > 0:
+				data = ser.readline().decode('utf-8').strip()
+				print(f"Received from interferometer: {data}")
+				
+				# Send data to TCP client
+				sock.sendall(data.encode('utf-8'))
+			else:
+				time.sleep(0.1)
+
+	except KeyboardInterrupt:
+		print("Interrupted by user")
+
+	finally:
+		# Clean up
+		connection.close()
+		server_socket.close()
+		ser.close()
--- server.py
+++ server.py
@@ -8,11 +8,13 @@ if __name__ == "__main__":
 	print("done.\nLoading hostname and IP ...", end="")
 	sys.stdout.flush()
 	hn = socket.gethostname()
-	ip = socket.gethostbyname()
+	ip = socket.gethostbyname(hn)
 	print("done, hostname", hn, "ip", ip, "\nAwaiting connection ...")
 	sock.bind((ip, 7777))
 	sock.listen(1)
 	c, a = sock.accept()
 	print("Got connection from", a[0] + ":" + str(a[1]) + ", receiving remote interferometer data ...")
-
+	while True:
+		d = c.recv(1024)
+		print(d.decode("utf-8"))
 	c.close()
