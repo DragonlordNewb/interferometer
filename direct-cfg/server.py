dfa76f74dc82d0edd0a8ab7fbde9458f86a53273
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2024-06-21 21:47:08 -0500

    rthrthsrth

---
+++ direct-cfg/server.py
@@ -0,0 +1,44 @@
+import socket
+import statistics
+import sys
+
+last100 = []
+buildingUp = True
+
+def mean():
+	return sum(last100) / 100
+
+def stdev():
+	return statistics.stdev(last100)
+
+if __name__ == "__main__":
+	print("Loading socket ...", end="")
+	sys.stdout.flush()
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	print("done.\nLoading hostname and IP ...", end="")
+	sys.stdout.flush()
+	hn = socket.gethostname()
+	ip = socket.gethostbyname(hn)
+	print("done, hostname", hn, "ip", ip, "\nAwaiting connection ...")
+	sock.bind((ip, 7777))
+	d = sock.recvfrom(1024)
+	print("Got packet, receiving remote interferometer data ...")
+	while True:
+		d = sock.recv(1024)
+		s = d.decode("utf-8")
+		i = 0
+		try:
+			i = float(s)
+		except:
+			print("Error: corrupted data packet")
+		if len(last100) < 100:
+			print("\rBuilding up baseline data (" + str(len(last100)) + "%) ...", end="")
+			last100.append(i)
+		else:
+			if buildingUp == True:
+				buildingUp = False
+				print("\rCollected baseline data points for analysis.")
+			print("New data point:", s, "- percent change", str(100 * ((i / last100[99]) - 1)) + "% - standard deviation", str((i - mean()) / stdev()))
+			last100.append(i)
+			last100.pop(0)
+	c.close()
