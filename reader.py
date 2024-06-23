17f33c3c2357bfdff98398ae29380bc8f54cecfd
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2024-06-22 20:43:20 -0500

    bleh

--- reader.py
+++ reader.py
@@ -102,63 +102,37 @@ def css(i):
 	return color["bold"] + color["purple"] + "F  █" + color["end"]
 
 if __name__ == "__main__":
-	print(color["white"] + "Loading socket ...", end="")
-	sys.stdout.flush()
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print("done.\nLoading hostname and IP ...", end="")
-	sys.stdout.flush()
-	hn = socket.gethostname()
-	ip = socket.gethostbyname(hn)
-	print("done, hostname", hn, "ip", ip, "\nAwaiting connection ...")
-	sock.bind((ip, 7777))
-	d = sock.recvfrom(1024)
-	print("Got packet, receiving remote interferometer data ...")
-	while True:
+	if len(sys.argv) != 2:
+		print("Invalid syntax: \"python3 reader.py <filename>\"")
+		exit(-1)
+	raw = None
+	lns = []
+	with open(sys.argv[1], "rb") as f:
+		raw = zlib.decompress(f.read())
+	lns = [float(i.strip()) for i in raw.decode("utf-8").split("\r\n") if i not in ["", " ", "\r", "\n", "\r\n"]]
+	for n in range(len(lns)):
 		try:
-			d = sock.recv(1024)
-			s = d.decode("utf-8")
-			i = 0
-			try:
-				i = float(s)
-			except:
-				print("Error: corrupted data packet")
+			i = lns[n]
 			if len(last100) < 100:
-				print("\rBuilding up baseline data (" + str(len(last100)) + "%) ...", end="")
 				last100.append(i)
 			else:
 				if buildingUp == True:
 					buildingUp = False
 					starttime = datetime.datetime.now(datetime.UTC).strftime("d%m%d%yt%H%M%S")
-					print("\rCollected baseline data points for analysis.\nStarting session.")
 				else:
 					overall += 1
 					alldps.append(i)
 				print(
-					color["white"] + ts() + "\t", overall, color["bold"] + "\tNew data point:" + color["end"] + color["white"], s, "\tpercent change", str(round(100 * ((i / last100[99]) - 1), 3)) + "%\tstd. deviation", str(round((i - mean()) / stdev(), 3)),
+					color["white"] + ts() + "\t", overall, color["bold"] + "\tNew data point:" + color["end"] + color["white"], str(i), "\tpercent change", str(round(100 * ((i / last100[99]) - 1), 3)) + "%\tstd. deviation", str(round((i - mean()) / stdev(), 3)),
 					"\tsignificance", sig(i), color["bold"] + color["white"] + "\t\tsignal coherence" + color["end"], scr(), color["white"] + "\tmean value", str(round(mean(), 3)), "\taverage std. deviation", 
 					str(round(stdev(), 3)), "\t\t\tcombined statistical significance:", css(i)
 				)
+				if not (n % 50):
+					input()
 				last100.append(i)
 				last100.pop(0)
 				last10.append((i - mean()) / stdev())
 				last10.pop(0)
 		except KeyboardInterrupt:
-			print(color["white"] + "Session finished, exporting data ...")
-			dfn = "session_" + starttime + ".interferometer.dat"
-			print("  Data file:", dfn)
-			print("  Data points:", len(alldps))
-			with open(dfn, "x") as f:
-				pass
-			with open(dfn, "a") as f:
-				for n, i in enumerate(alldps):
-					f.write(str(i) + "\n")
-					print("\rWriting data ... (" + str(n+1) + "/" + str(len(alldps)) + " " + str(100 * round((n + 1) / len(alldps), 3)) + "%) ", end="")
-			print("done.                                   \n  Data written. Compressing ...", end="")
-			raw = None
-			with open(dfn, "rb") as f:
-				raw = f.read()
-			with open(dfn, "wb") as f:
-				f.write(zlib.compress(raw))
-			print("done.\nData successfully exported.")
 			break
 	print(color["end"])
--- server.py
+++ server.py
@@ -2,10 +2,27 @@ import socket
 import statistics
 import sys
 import datetime
+import zlib
 
-last100 = [x for x in range(100)]
-last10 = [0.1 for _ in range(10)]
+alldps = []
+last100 = [0, 1]
+overall = 0
+last10 = [10000 for _ in range(10)]
 buildingUp = True
+
+color = {
+	'white': "\033[1;37m",
+	"purple": '\033[95m',
+	"cyan": '\033[96m',
+	"darkcyan": '\033[36m',
+	"blue": '\033[94m',
+	"green": '\033[92m',
+	"yellow": '\033[93m',
+	"red": '\033[91m',
+	"bold": '\033[1m',
+	"underline": '\033[4m',
+	"end": '\033[0m'
+}
 
 def mean():
 	return sum(last100) / 100
@@ -13,74 +30,139 @@ def mean():
 def stdev():
 	return statistics.stdev(last100)
 
+def ts():
+	return datetime.datetime.now(datetime.UTC).strftime("%m/%d/%y %H:%M:%S.%f UTC")
+
+sigi = 0
+scri = 0
+starttime = None
+
 def scr():
-	sd = (sum(last10)/10)
-	if sd > 2:
-		return "F"
-	if sd > 1:
-		return "D"
-	if sd > .5:
-		return "C"
-	if sd > .1:
-		return "B"
-	if sd > .05:
-		return "A"
-	return "S"
+	global scri
+	if len(last10) < 10:
+		return color["blue"] + "?" + color["end"]
+	k = sum([abs(i) for i in last10]) / 10
+	if k > 3:
+		scri = 0.1
+		return color["purple"] + "F" + color["end"]
+	if k > 2:
+		scri = 0.25
+		return color["red"] + "D" + color["end"]
+	if k > 1:
+		scri = 0.5
+		return color["yellow"] + "C" + color["end"]
+	if k > 0.5:
+		scri = 1
+		return color["green"] + "B" + color["end"]
+	if k > 0.1:
+		scri = 2.5
+		return color["cyan"] + "A" + color["end"]
+	if k > 0.05:
+		scri = 5
+		return color["underline"] + color["cyan"] + "S" + color["end"]		
 
 def sig(i):
-	if i > 6:
-		return "AAA"
-	if i > 5:
-		return "A"
-	if i > 4:
-		return "B"
-	if i > 3:
-		return "C"
-	if i > 2:
-		return "D"
-	return "F"
+	global sigi
+	k = round((i - mean()) / stdev(), 3)
+	if k > 7.5:
+		sigi = 5
+		return color["underline"] + color["cyan"] + "S" + color["end"]
+	if k > 5:
+		sigi = 2.5
+		return color["cyan"] + "A" + color["end"]
+	if k > 4:
+		sigi = 1
+		return color["green"] + "B" + color["end"]
+	if k > 3:
+		sigi = 0.5
+		return color["yellow"] + "C" + color["end"]
+	if k > 2:
+		sigi = 0.25
+		return color["red"] + "D" + color["end"]
+	sigi = 0
+	return color["purple"] + "F" + color["end"]
 
-def current_time_string():
+def css(i):
+	k = round(sigi * scri * 4, 2)
 
-    now = datetime.datetime.now()
-    formatted_time = now.strftime("%m/%d/%Y %H:%M:%S.%f")[:-3]  # Format as mm/dd/yyyy xx:xx:xx.xxx
-    return formatted_time
+	if k == 100: return color["bold"] + color["underline"] + color["cyan"] +  "S  ███████████████" + color["end"]
+	if k == 50.0: return color["bold"] + color["cyan"] + "A+ ██████████████" + color["end"]
+	if k == 25.0: return color["bold"] + color["cyan"] + "A  █████████████" + color["end"]
+	if k == 20: return color["bold"] + color["cyan"] + "A- ████████████" + color["end"]
+	if k == 10.0: return color["bold"] + color["green"] + "B+ ███████████" + color["end"]
+	if k == 5.0: return color["bold"] + color["green"] + "B  ██████████" + color["end"]
+	if k == 4: return color["bold"] + color["green"] + "B- █████████" + color["end"]
+	if k == 2.5: return color["bold"] + color["yellow"] + "C+ ████████" + color["end"]
+	if k == 2.0: return color["bold"] + color["yellow"] + "C  ███████" + color["end"]
+	if k == 1.0: return color["bold"] + color["yellow"] + "C- ██████" + color["end"]
+	if k == 0.5: return color["bold"] + color["red"] + "D+ █████" + color["end"]
+	if k == 0.4: return color["bold"] + color["red"] + "D  ████" + color["end"]
+	if k == 0.25: return color["bold"] + color["red"] + "D- ███" + color["end"]
+	if k == 0.2: return color["bold"] + color["purple"] + "F+ ██" + color["end"]
+	return color["bold"] + color["purple"] + "F  █" + color["end"]
 
 if __name__ == "__main__":
-	print("Loading socket ...", end="")
+	print(color["white"] + "Loading socket ...", end="")
 	sys.stdout.flush()
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	print("done.\nLoading hostname and IP ...", end="")
 	sys.stdout.flush()
 	hn = socket.gethostname()
 	ip = socket.gethostbyname(hn)
-	print("done, hostname", hn, "ip", ip, "\nAwaiting data ...")
+	print("done, hostname", hn, "ip", ip, "\nAwaiting connection ...")
 	sock.bind((ip, 7777))
 	d = sock.recvfrom(1024)
+	print("Got packet, receiving remote interferometer data ...")
 	while True:
-		d = sock.recv(1024)
-		s = d.decode("utf-8")
-		i = 0
 		try:
-			i = float(s)
-		except:
-			print("Error: corrupted data packet")
-		if len(last100) < 100 and "skip" not in sys.argv:
-			print("\rBuilding up baseline data (" + str(len(last100)) + "%) ...", end="")
-			last100.append(i)
-		else:
-			if buildingUp == True:
-				buildingUp = False
-				print("\rCollected baseline data points for analysis.")
-			if last100[99] != 0:
-				print(
-					current_time_string(), "\t New data point:", s, "\t\t percent change", str(round(100 * ((i / last100[99]) - 1), 3)) + "%\t\t standard deviations:", str(round((i - mean()) / stdev(), 3)),
-					"\tcurrent statistics:\tmean:", str(round(mean(), 3)), "\tstandard deviation:", str(round(stdev(), 3)), "\tsignal coherence:", scr(), "\tsignificance:", sig(round((i - mean()) / stdev(), 3))
-				)
+			d = sock.recv(1024)
+			s = d.decode("utf-8")
+			i = 0
+			try:
+				i = float(s)
+			except:
+				print("Error: corrupted data packet")
+			if len(last100) < 100:
+				print("\rBuilding up baseline data (" + str(len(last100)) + "%) ...", end="")
+				last100.append(i)
 			else:
-				print("Error: interferometer system failure")
-			last100.append(i)
-			last100.pop(0)
-			last10.append(abs(stdev()))
-			last10.pop(0)
-	c.close()
+				if buildingUp == True:
+					buildingUp = False
+					starttime = datetime.datetime.now(datetime.UTC).strftime("d%m%d%yt%H%M%S")
+					print("\rCollected baseline data points for analysis.\nStarting session.")
+				else:
+					overall += 1
+					alldps.append(i)
+				print(
+					color["white"] + ts() + "\t", overall, color["bold"] + "\tNew data point:" + color["end"] + color["white"], s, "\tpercent change", str(round(100 * ((i / last100[99]) - 1), 3)) + "%\tstd. deviation", str(round((i - mean()) / stdev(), 3)),
+					"\tsignificance", sig(i), color["bold"] + color["white"] + "\t\tsignal coherence" + color["end"], scr(), color["white"] + "\tmean value", str(round(mean(), 3)), "\taverage std. deviation", 
+					str(round(stdev(), 3)), "\t\t\tcombined statistical significance:", css(i)
+				)
+				last100.append(i)
+				last100.pop(0)
+				last10.append((i - mean()) / stdev())
+				last10.pop(0)
+		except KeyboardInterrupt:
+			print(color["white"] + "Session finished, exporting data ...")
+			note = input("Notes on this session: ")
+			addendum = ""
+			if note != "":
+				addendum = "_" + note
+			dfn = "session_" + starttime + addendum + ".interferometer.dat"
+			print("  Data file:", dfn)
+			print("  Data points:", len(alldps))
+			with open(dfn, "x") as f:
+				pass
+			with open(dfn, "a") as f:
+				for n, i in enumerate(alldps):
+					f.write(str(i) + "\n")
+					print("\rWriting data ... (" + str(n+1) + "/" + str(len(alldps)) + " " + str(100 * round((n + 1) / len(alldps), 3)) + "%) ", end="")
+			print("done.                                   \n  Data written. Compressing ...", end="")
+			raw = None
+			with open(dfn, "rb") as f:
+				raw = f.read()
+			with open(dfn, "wb") as f:
+				f.write(zlib.compress(raw))
+			print("done.\nData successfully exported.")
+			break
+	print(color["end"])
